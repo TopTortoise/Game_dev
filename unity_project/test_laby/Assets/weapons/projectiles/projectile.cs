599f4eac90566c968ffe8cec8d384dd327c84672
TopTortoise <paul.b00o3721@gmail.com>
2025-11-29 11:13:54 +0100

    added red staff

--- unity_project/test_laby/Assets/weapons/projectiles/projectile.cs
+++ unity_project/test_laby/Assets/weapons/projectiles/projectile.cs
@@ -10,17 +10,32 @@ public class projectile : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle+90);
+        // transform.rotation = Quaternion.Euler(0,0, Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg - 90);
         Invoke("DestroyProjectile", lifetime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(direction * speed * Time.deltaTime);
+        transform.Translate(direction * speed * Time.deltaTime, Space.World);
     }
 
     void DestroyProjectile()
     {
         Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("hit soemthing " + collider);
+        IKillable obj = collider.gameObject.GetComponent<IKillable>();
+        if (obj != null)
+        {
+            obj.hit(damage);
+        }
+        DestroyProjectile();
     }
 }
