e66ed6c6cd852e1b69b35bce0b6448f05ecace89
Jona Rodleitner <127888655+jRodleitner@users.noreply.github.com>
2026-01-13 00:02:10 +0100

    Title Screen links to MainScene + Music update

--- unity_project/test_laby/Assets/Scenes/utility/TitleScreen.cs
+++ unity_project/test_laby/Assets/Scenes/utility/TitleScreen.cs
@@ -5,7 +5,7 @@ public class TitleScreen : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("JonaScene"); // change name if needed
+        SceneManager.LoadScene("MainScene"); // change name if needed
     }
 
     public void QuitGame()
