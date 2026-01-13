dc03fb1eb8d5ccf15b874816019257ead9e6726b
Tobias <tobiaslettner444@gmail.com>
2026-01-13 10:49:16 +0100

    clock Edit

--- unity_project/test_laby/Assets/Scenes/Ui/Clock.cs
+++ unity_project/test_laby/Assets/Scenes/Ui/Clock.cs
@@ -2,93 +2,99 @@ using UnityEngine;
 using TMPro;
 
 public class Clock : MonoBehaviour
-{   
-    public int duration = 300; 
-    public int warningTime = 60; 
-    public int timeRemaining; 
+{
+    public int duration = 300;
+    public int warningTime = 60;
+    public int timeRemaining;
     public bool isCountingDown = true;
     private bool warningStarted = false;
     private Enemy_Manager em;
     public TMP_Text clockText;
     public ghost player;
     public static Clock Instance;
-    
-    public void Start(){
-        Instance = this;
-        timeRemaining = duration;
-        isCountingDown = true;
-        Invoke("_tick", 1f);
-        em = GameObject.FindFirstObjectByType<Enemy_Manager>();
 
+    public void Start()
+    {
+        Instance = this;
+       
+        StartTimer();
+        em = GameObject.FindFirstObjectByType<Enemy_Manager>();
     }
 
     public void StartTimer()
     {
+        CancelInvoke("_tick"); 
         timeRemaining = duration;
         warningStarted = false;
         isCountingDown = true;
+        
+      
+        if (clockText != null) clockText.color = Color.white;
+        if (player != null) player.ChangeSpotlight(1f);
+
         Invoke("_tick", 1f);
     }
 
-    private void _tick() 
+    private void _tick()
     {
         timeRemaining--;
-        
-        // NUR wenn unter 1 Minute: Starte die Dunkelheit
+
+       
         if (timeRemaining <= warningTime && player != null)
         {
             if (!warningStarted)
             {
                 warningStarted = true;
-                // Debug.Log("WARNUNG: Weniger als 1 Minute! Es wird dunkel!");
             }
-            
-          
+
             float t = Mathf.Clamp01((float)timeRemaining / warningTime);
-            player.ChangeSpotlight(t);  
-            
-            // Optional: Ändere die Textfarbe zur Warnung
+            player.ChangeSpotlight(t);
+
+          
             if (clockText != null)
             {
                 clockText.color = Color.Lerp(Color.red, Color.white, t);
             }
         }
+
         
-        // Update den Timer-Text
-        if(timeRemaining > 0) 
+        if (timeRemaining > 0)
         {
-            int minutes = timeRemaining / 60; 
+            int minutes = timeRemaining / 60;
             int seconds = timeRemaining - minutes * 60;
             if (clockText != null)
             {
                 clockText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
             }
             Invoke("_tick", 1f);
-        } 
-        else 
+        }
+        else
         {
+           
             isCountingDown = false;
-            // Debug.Log("ZEIT ABGELAUFEN! KAMPF BEGINNT!");
-            
-            
+
             if (player != null)
             {
-                player.ChangeSpotlight(0f); // Komplett dunkel
+               
+                player.ChangeSpotlight(1f); 
+                
                 player.transform.position = player.spawn_pos;
                 // StartCoroutine(em.spawnWave());
             }
-            // TODO: Spawne Gegner oder starte Kampf-Event
+
+            
+            if (clockText != null)
+            {
+                clockText.color = Color.white;
+            }
         }
     }
-    
+
     public void ResetTimer()
     {
-        isCountingDown = false;
-        warningStarted = false;
         StartTimer();
     }
-    
-    // Gibt zurück ob die Warnung aktiv ist
+
     public bool IsWarningActive()
     {
         return timeRemaining <= warningTime && timeRemaining > 0;
