9cbdcbb0a9aabf154f4583db18bd2326af22e0fb
Tobias <tobiaslettner444@gmail.com>
2026-01-21 23:46:07 +0100

    added Pause and Control overlay

---
+++ unity_project/test_laby/Assets/Scenes/Ui/PauseMenuManager.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    private bool isPaused = false;
+
+    public GameObject pauseMenuPanel;
+    public GameObject controlsPanel; 
+    private bool controlActive =  false; 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        pauseMenuPanel.SetActive(false);
+        controlsPanel.SetActive(false); 
+    }
+
+       public void TogglePause()
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        if (pauseMenuPanel != null)
+        {
+            pauseMenuPanel.SetActive(true); // Menü an
+            Time.timeScale = 0f;            // Zeit stopp
+            isPaused = true;
+        }
+    }
+
+    public void ResumeGame()
+    {
+        if (pauseMenuPanel != null)
+        {
+            pauseMenuPanel.SetActive(false); // Menü aus
+            Time.timeScale = 1f;             // Zeit läuft
+            isPaused = false;
+        }
+    }
+
+    public void ActivateControls()
+    {
+        if (controlActive)
+        {
+            controlsPanel.SetActive(false);
+            controlActive = false;
+        }
+        else
+        {
+            controlsPanel.SetActive(true);
+            controlActive = true;
+        }
+    }
+    
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+}
